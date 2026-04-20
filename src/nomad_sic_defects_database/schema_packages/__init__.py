6c3cdfdb71895ea873bbef5e8eacf939126b46ea
ninahaelterlein <nina.haelterlein@fau.de>
2026-04-20 16:07:50 +0200

    added defect schema

--- src/nomad_sic_defects_database/schema_packages/__init__.py
+++ src/nomad_sic_defects_database/schema_packages/__init__.py
@@ -2,16 +2,15 @@ from nomad.config.models.plugins import SchemaPackageEntryPoint
 from pydantic import Field
 
 
-class NewSchemaPackageEntryPoint(SchemaPackageEntryPoint):
-    parameter: int = Field(0, description='Custom configuration parameter')
+class DefectSchemaPackageEntryPoint(SchemaPackageEntryPoint):
 
     def load(self):
-        from nomad_sic_defects_database.schema_packages.schema_package import m_package
+        from nomad_sic_defects_database.schema_packages.defect_schema import m_package
 
         return m_package
 
 
-schema_package_entry_point = NewSchemaPackageEntryPoint(
-    name='NewSchemaPackage',
-    description='New schema package entry point configuration.',
+defect = DefectSchemaPackageEntryPoint(
+    name='DefectSchemaPackage',
+    description='Defect schema package entry point configuration.',
 )
---
+++ src/nomad_sic_defects_database/schema_packages/defect_schema.py
@@ -0,0 +1,85 @@
+from typing import (
+    TYPE_CHECKING,
+)
+
+if TYPE_CHECKING:
+    from nomad.datamodel.datamodel import (
+        EntryArchive,
+    )
+    from structlog.stdlib import (
+        BoundLogger,
+    )
+
+from nomad.config import config
+from nomad.datamodel.data import Schema
+from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
+from nomad.metainfo import Quantity, SchemaPackage
+
+configuration = config.get_plugin_entry_point(
+    'nomad_sic_defects_database.schema_packages:schema_package_entry_point'
+)
+
+m_package = SchemaPackage()
+
+
+class SiCDefect(Schema):
+    m_def = Section(
+        a_eln=ELNAnnotation(
+            propertiers=SectionProperties(
+                order=[
+                    'defect_name',
+                    'defect_type',
+                    'defect_charge',
+                    'defect_leverl',
+                ],
+            )
+        )
+    )
+
+    defect_name = Quantity(
+        type=str,
+        description='Name of the defect',
+        a_eln=ELNAnnotation(
+            component = RichTextEditQuantity,
+            label = 'Defect Name',
+        ),
+    )
+
+    defect_type = Quantity(
+        type=str,
+        description='Type of the defect',
+        a_eln=ELNAnnotation(
+            component = ELNComponentEnum,
+            label = 'Defect Type',
+            options = ['Vacancy', 'Interstitial', 'Substitutional'],
+        ),
+    )   
+
+    defect_charge = Quantity(
+        type=int,
+        description='Charge of the defect',
+        a_eln=ELNAnnotation(
+            component = ELNComponentEnum,
+            label = 'Defect Charge',
+            options = [-2, -1, 0, 1, 2],
+        ),
+    )
+
+    defect_level = Quantity(
+        type=float,
+        description='Energy level of the defect in eV',
+        a_eln=ELNAnnotation(
+            component = ELNComponentEnum,
+            label = 'Defect Level (eV)',
+        ),
+    )
+
+
+    def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
+        super().normalize(archive, logger)
+
+        logger.info('NewSchema.normalize', parameter=configuration.parameter)
+        self.message = f'Hello {self.name}!'
+
+
+m_package.__init_metainfo__()
